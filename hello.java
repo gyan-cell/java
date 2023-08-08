cbe35ffcecf2b6252eae646ef21fd5a80a49a9fd
gyan-cell <jha07944@gmail.com>
2023-08-08 22:32:48 +0530

    Hello World In java

---
+++ hello.java
@@ -0,0 +1,12 @@
+/**
+ * hello
+ */
+// Java is different in that it does not compile the code into machine language code.
+// Compilation creates bytecode out of the source code. Source code is written and stored in files with the extension of .java,
+// and after compilation a file with a ".class" extension is created which contains 
+// the bytecode instructions. Bytecode generally looks something like hello.class file 
+public class hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World To java From Gyan...");
+  }
+}
