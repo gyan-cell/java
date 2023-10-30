0d5a412498851744a60fb5d1aba1b9cfde17de54
gyan-cell <jha07944@gmail.com>
2023-10-30 08:06:47 +0530

    Packages In Java

---
+++ packages.java
@@ -0,0 +1,18 @@
+/**
+ * packages
+ */
+
+
+// Note static keyword after import. 
+import static java.lang.System.*; 
+   
+class packages 
+{ 
+   public static void main(String args[]) 
+   {       
+        // We don't need to use 'System.out'  
+        // as imported using static. 
+        out.println("GeeksforGeeks"); 
+   } 
+} 
+
