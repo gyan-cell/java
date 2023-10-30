0d7a8fdcdcd3ebf9c1d38b19495f06afc4745faa
gyan-cell <jha07944@gmail.com>
2023-10-30 08:11:11 +0530

    Exception Handling in Java

---
+++ ExceptionHandling.java
@@ -0,0 +1,14 @@
+/**
+ * ExceptionHandling
+ */
+public class ExceptionHandling {
+
+  public static void main(String[] args) {
+    try {
+      int data = 100 / 0;
+    } catch (ArithmeticException e) {
+      System.out.println(e);
+    }
+    System.out.println("Proof That Exception in Handled!....");
+  }
+}
