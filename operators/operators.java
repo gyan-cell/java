d93cdd39e5cb94251b25ee7441307a3cb3d6d4c4
gyan-cell <jha07944@gmail.com>
2025-05-10 21:50:39 +0530

    Operators in java

---
+++ operators/operators.java
@@ -0,0 +1,63 @@
+// Java operators are special symbols that perform operations on variables or values. These operators 
+// are essential in programming as they allow you to manipulate data efficiently. They can be classified 
+// into different categories based on their functionality. In this article, we 
+// will explore different types of operators in Java, including arithmetic, unary, relational, 
+// logical, and more, along with practical examples. 
+
+class Operator {
+  public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+
+    System.out.println("The Sum is " + sum);
+
+    int diff = num1 - num2;
+
+    System.out.println("The Difference is " + diff);
+
+    int a = 10;
+    int b = 3;
+
+    // Arithmetic operators on Strings
+    String n1 = "15";
+    String n2 = "25";
+
+    // Convert Strings to integers
+    int a1 = Integer.parseInt(n1);
+    int b1 = Integer.parseInt(n2);
+
+    System.out.println("a + b = " + (a + b));
+    System.out.println("a - b = " + (a - b));
+    System.out.println("a * b = " + (a * b));
+    System.out.println("a / b = " + (a / b));
+    System.out.println("a % b = " + (a % b));
+    System.out.println("a1 + b1 = " + (a1 + b1));
+
+  }
+}
+
+// Types of Operators in Java
+//
+// Arithmetic Operators
+// Unary Operators
+// Assignment Operator
+// Relational Operators
+// Logical Operators
+// Ternary Operator
+// Bitwise Operators
+// Shift Operators
+// instance of operator
+//
+
+// 1. Arithmetic Operators
+//
+// Arithmetic Operators are used to perform simple arithmetic operations on
+// primitive and non-primitive data types.
+//
+// * : Multiplication
+// / : Division
+// % : Modulo
+// + : Addition
+// – : Subtraction
+//
